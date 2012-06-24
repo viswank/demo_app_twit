6d7d8b55e00230b688e9fb10af94771c624e874e
Karun Viswanath <karunv@gmail.com>
2012-06-24 08:44:27 -0700

    added a static pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 DemoAppTwit::Application.routes.draw do
+  get "static_pages/home"
+
+  get "static_pages/help"
+
+  resources :microposts
+
+  resources :users
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
